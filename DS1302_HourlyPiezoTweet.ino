1808e50e8439918484f6b94de8859d3b9cbe7916
Suren Bono <surenbono76@gmail.com>
2022-10-23 07:59:43 +0800

    Create DS1302_HourlyPiezoTweet.ino
    Battery Memory Real Time Clock with Hourly chimes...time is Gold!

---
+++ DS1302_HourlyPiezoTweet.ino
@@ -0,0 +1,120 @@
+  /* 
+Modified on Nov 25, 2020
+Modified by MehranMaleki from Arduino Examples
+https://electropeak.com/learn/interfacing-ds1302-real-time-clock-rtc-module-with-arduino/
+RTC module with CR2032 coin battery
+
+*/
+
+
+// CONNECTIONS:
+// DS1302 CLK/SCLK --> 5
+// DS1302 DAT/IO --> 4
+// DS1302 RST/CE --> 2
+// DS1302 VCC --> 3.3v - 5v
+// DS1302 GND --> GND
+// Piezo  GND & VCC --> 3 
+
+#include <ThreeWire.h>  
+#include <RtcDS1302.h>
+
+const int P= 3; //Piezo VCC
+int f= 4000;    //Piezo Frequency 
+
+ThreeWire myWire(4,5,2); // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
+
+void setup () 
+{
+    Serial.begin(9600);
+    pinmode(P,OUTPUT);    // Piezo init
+    Serial.print("compiled: ");
+    Serial.print(__DATE__);
+    Serial.println(__TIME__);
+
+    Rtc.Begin();
+
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+    printDateTime(compiled);
+    Serial.println();
+
+    if (!Rtc.IsDateTimeValid()) 
+    {
+        // Common Causes:
+        //    1) first time you ran and the device wasn't running yet
+        //    2) the battery on the device is low or even missing
+
+        Serial.println("RTC lost confidence in the DateTime!");
+        Rtc.SetDateTime(compiled);
+    }
+
+    if (Rtc.GetIsWriteProtected())
+    {
+        Serial.println("RTC was write protected, enabling writing now");
+        Rtc.SetIsWriteProtected(false);
+    }
+
+    if (!Rtc.GetIsRunning())
+    {
+        Serial.println("RTC was not actively running, starting now");
+        Rtc.SetIsRunning(true);
+    }
+
+    RtcDateTime now = Rtc.GetDateTime();
+    if (now < compiled) 
+    {
+        Serial.println("RTC is older than compile time!  (Updating DateTime)");
+        Rtc.SetDateTime(compiled);
+    }
+    else if (now > compiled) 
+    {
+        Serial.println("RTC is newer than compile time. (this is expected)");
+    }
+    else if (now == compiled) 
+    {
+        Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+    }
+}
+
+void loop () 
+{
+    RtcDateTime now = Rtc.GetDateTime();
+
+    printDateTime(now);
+    Serial.println();
+
+    if (!now.IsValid())
+    {
+        // Common Causes:
+        //    1) the battery on the device is low or even missing and the power line was disconnected
+        Serial.println("RTC lost confidence in the DateTime!");
+    }
+
+    delay(5000); // five seconds
+}
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            dt.Month(),
+            dt.Day(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+    Serial.print(datestring);
+
+if (dt.Minute()==00 && dt.Second()==00){
+tone(P,f);delay(50);notone(P);delay(50);
+tone(P,f);delay(50);notone(P);}  // Beep twice hourly 
+
+if (dt.Minute()==30 && dt.Second()==00){
+tone(P,f);delay(50);notone(P);}  // Once every 30 min
+
+}
